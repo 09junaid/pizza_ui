43ccdfea40fbd1f345297f0234de8d26e0ee585b
Junaid Arshad <junaidarshad647@gmail.com>
2025-09-01 12:14:10 +0500

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="p-8 space-y-4">
       <h1 className="text-3xl font-bold">Welcome to Pizza Shop</h1>
-    <Button variant={"outline"}>Order Now</Button>
-        </div>
+      <Button>Order Now</Button>
+    </div>
   );
 }
